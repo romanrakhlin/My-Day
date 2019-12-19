ff021408b0137f8afaf99c4374c76cb0767f0d18
Roman <rrakhlin@gmail.com>
2019-12-19 12:06:04 +0300

    Initial Commit

---
+++ My Day/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Day
+//
+//  Created by Roman Rakhlin on 19.12.2019.
+//  Copyright © 2019 Roman Rakhlin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
